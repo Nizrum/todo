bab8d7f79940d340432271a7be38a59e6ccb83e8
Nizrum <62435919+Nizrum@users.noreply.github.com>
2022-08-09 12:48:42 +0400

    Небольшие изменения
    Добавлена возможность добавлять задачи по нажатию на клавишу Enter. Добавлен favicon.

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const input = document.querySelector(".task-input__input");
 const addButton = document.querySelector(".task-input__button");
 const tasksList = document.querySelector(".tasks__list");
 
-addButton.addEventListener("click", function () {
+function addTask() {
   if (input.value != '') {
     tasksList.insertAdjacentHTML(
       "beforeend", `<li class="tasks__task task"><span class="task__text">${input.value}</span><button class="task__button"><img src="/images/check.svg" alt="X"></button></li>`
@@ -12,4 +12,11 @@ addButton.addEventListener("click", function () {
         this.closest(".task").remove();
     });
   }
+}
+
+addButton.addEventListener("click", addTask);
+input.addEventListener('keydown', function (event) {
+    if (event.key == 'Enter') {
+        addTask();
+    }
 });

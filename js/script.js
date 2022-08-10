8d76af7189eae1414b678d75000cca1917f66651
Nizrum <62435919+Nizrum@users.noreply.github.com>
2022-08-10 12:23:46 +0400

    Задания сохраняются в localStorage
    Задания сохраняются в localStorage и отображаются после перезагрузки страницы.

--- js/script.js
+++ js/script.js
@@ -2,21 +2,42 @@ const input = document.querySelector(".task-input__input");
 const addButton = document.querySelector(".task-input__button");
 const tasksList = document.querySelector(".tasks__list");
 
-function addTask() {
-  if (input.value != '') {
+let tasks = localStorage.getItem("tasks") ? localStorage.getItem("tasks").split(",") : [];
+
+for (let t of tasks) {
     tasksList.insertAdjacentHTML(
-      "beforeend", `<li class="tasks__task task"><span class="task__text">${input.value}</span><button class="task__button"><img src="/images/check.svg" alt="X"></button></li>`
+      "beforeend",
+      `<li class="tasks__task task"><span class="task__text">${t}</span><button class="task__button"><img src="/images/check.svg" alt="X"></button></li>`
     );
-    input.value = '';
-    document.querySelector(`.task:last-child .task__button`).addEventListener("click", function () {
-        this.closest(".task").remove();
-    });
+}
+
+function addTask(data) {
+  if (data != '') {
+    tasksList.insertAdjacentHTML(
+      "beforeend", `<li class="tasks__task task"><span class="task__text">${data}</span><button class="task__button"><img src="/images/check.svg" alt="X"></button></li>`
+    );
+    tasks.push(data);
+    localStorage.setItem('tasks', tasks);
   }
 }
 
-addButton.addEventListener("click", addTask);
+tasksList.addEventListener('click', function (event) {
+    let li = event.target.closest('.task');
+    if (li && event.target.closest('button')) {
+        tasks.splice(tasks.indexOf(li.textContent), 1);
+        localStorage.setItem("tasks", tasks);
+        li.remove();
+    }
+});
+
+addButton.addEventListener("click", function () {
+    addTask(input.value);
+    input.value = "";
+});
+
 input.addEventListener('keydown', function (event) {
     if (event.key == 'Enter') {
-        addTask();
+        addTask(input.value);
+        input.value = "";
     }
 });
